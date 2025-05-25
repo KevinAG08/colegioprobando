b2f50871c6046e5ff8cd909e5b015f25a834cf2a
KevinAG08 <75378802@continental.edu.pe>
2025-05-25 15:31:09 -0500

    update total

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,19 +2,18 @@ import express, { Express } from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
-import authRoutes from './routes/auth';
-import adminRoutes from './routes/admin';
-import aulasRoutes from './routes/aula';
-import profesorRoutes from './routes/profesor';
-import estudianteRoutes from './routes/estudiante';
-import incidenciaRoutes from './routes/incidencia';
-import asistenciaRoutes from './routes/asistencia';
-import dashboardRoutes from './routes/dashboard';
+import authRoutes from '../routes/auth';
+import adminRoutes from '../routes/admin';
+import aulasRoutes from '../routes/aula';
+import profesorRoutes from '../routes/profesor';
+import estudianteRoutes from '../routes/estudiante';
+import incidenciaRoutes from '../routes/incidencia';
+import asistenciaRoutes from '../routes/asistencia';
+import dashboardRoutes from '../routes/dashboard';
 
 dotenv.config();
 
 const app: Express = express();
-const PORT = process.env.PORT || 5000;
 
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:5173";
 
@@ -35,6 +34,11 @@ app.use("/incidencias", incidenciaRoutes);
 app.use("/asistencias", asistenciaRoutes);
 app.use("/dashboard", dashboardRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-})
+module.exports = app;
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT_LOCAL = process.env.PORT || 5000;
+  app.listen(PORT_LOCAL, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT_LOCAL}`);
+  });
+}

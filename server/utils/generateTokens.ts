10a76f7bd54f13c125487c503d4deca21117271e
KevinAG08 <75378802@continental.edu.pe>
2025-06-01 19:28:01 -0500

    Update Token

--- server/utils/generateTokens.ts
+++ server/utils/generateTokens.ts
@@ -4,7 +4,7 @@ import crypto from "crypto";
 import { User, Estudiante } from "@prisma/client";
 
 const JWT_SECRET = process.env.JWT_SECRET || "my_jwt_super_secret_key";
-const ACCESS_TOKEN_EXPIRES_IN = "15m";
+const ACCESS_TOKEN_EXPIRES_IN = "24h";
 const REFRESH_TOKEN_EXPIRATION_DAYS = 30;
 
 export const generateTokens = async (
